c929b4710b5c0b1016948681ed71d1ae08eb3351
Hippo <badrihippo@gmail.com>
2016-01-02 18:36:44 +0530

    Add dummy `send_email` function for testing purposes
    TODO: Implement actual email sending using Flask-Email.
    
    Why am I not implementing it straight away, right now? Because the
    Internet speed is very slow and pip is just not able to download
    anything.

---
+++ flashcash/util/email.py
@@ -0,0 +1,9 @@
+from app import app
+def send_email(email, subject, message):
+    if app.config['DEBUG']:
+        print '==== Sending new email ===='
+        print 'To: %s' % email
+        print 'Subject: %s' % subject
+        print message
+        print '======= End of email ======='
+    # TODO: Add actual email-sending code here!

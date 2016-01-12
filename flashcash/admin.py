2519ba6b76a7b5a9b40417c5cbb555ec2df38511
Hippo <badrihippo@gmail.com>
2016-01-12 17:10:39 +0530

    Add Payment Portals (`Portal` model) to admin

--- flashcash/admin.py
+++ flashcash/admin.py
@@ -2,9 +2,10 @@ from flask_admin import Admin
 from flask_admin.contrib.peewee import ModelView
 
 from app import app, db
-from models import User, Branch, Note
+from models import User, Branch, Note, Portal
 
 admin = Admin(app)
 admin.add_view(ModelView(User, name='Users'))
+admin.add_view(ModelView(Portal, name='Payment Portals'))
 admin.add_view(ModelView(Branch, name='Branches'))
 admin.add_view(ModelView(Note, name='Notes'))

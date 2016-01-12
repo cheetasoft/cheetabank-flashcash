ecb1991d700ee3bf25e195fa70227502d91f8f55
Hippo <badrihippo@gmail.com>
2016-01-12 17:11:16 +0530

    Set __unicode__ methods to pretty-print models
    The value of __unicode__ will appear in places like the Admin site
    to represent models (instead of an unhelpful <Model:...>-style string)

--- flashcash/models.py
+++ flashcash/models.py
@@ -14,16 +14,32 @@ class User(db.Model, UserMixin):
     password = pw.CharField(max_length=128)
     email = pw.CharField(null=True)
     email_confirmed = pw.BooleanField(default=False)
+    def __unicode__(self):
+        return '%(username)s - %(name)s' % {
+            'name': self.name,
+            'username': self.username
+        }
 
 class Portal(db.Model):
     portal_code = pw.CharField(max_length=2)
     shop_name = pw.CharField(max_length=32)
-    owner = pw.ForeignKeyField(User)
+    owner = pw.ForeignKeyField(User, related_name='portals')
     balance = pw.DecimalField(decimal_places=2, default=0)
+
+    def __unicode__(self):
+        return '%(portal_code)s - %(shop_name)s' % {
+            'portal_code': self.portal_code,
+            'shop_name': self.shop_name
+        }
 
 class Branch(db.Model):
     id = pw.CharField(max_length=6, primary_key=True)
     name = pw.CharField(max_length=32)
+    def __unicode__(self):
+        return '%(id)s - %(name)s' % {
+            'id': self.id,
+            'name': self.name
+        }
 
 class Note(db.Model):
     note_id = pw.CharField(max_length=6)
@@ -31,3 +47,8 @@ class Note(db.Model):
     branch = pw.ForeignKeyField(Branch)
     value = pw.DecimalField(decimal_places=2, default=0)
     claimer = pw.ForeignKeyField(User, null=True)
+    def __unicode__(self):
+        return '%(note_id)s - %(value)d SSS' % {
+            'note_id': self.note_id,
+            'value': self.value
+        }

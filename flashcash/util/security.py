c4bca10513eb1255e6af8f9456e92330bfeff8a0
Hippo <badrihippo@gmail.com>
2016-04-03 15:18:32 +0530

    Add random ID generator function

--- flashcash/util/security.py
+++ flashcash/util/security.py
@@ -1,3 +1,9 @@
 from itsdangerous import URLSafeTimedSerializer
+import string
+import random
 from ..app import app
 ts = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+
+def random_id(size=6, chars=string.ascii_letters + string.digits):
+    '''Generate a random string used for FlashCash notecodes'''
+    return ''.join(random.choice(chars) for _ in range(size))

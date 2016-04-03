cad05d8d6fe2c6a242388bb203094b1580a1abdc
Hippo <badrihippo@gmail.com>
2016-04-03 12:20:32 +0530

    Make in-built config into fallback-only
    Now, in-built config is used only if no other config file exists.

--- flashcash/app.py
+++ flashcash/app.py
@@ -2,7 +2,10 @@ from flask import Flask
 from flask.ext.peewee.db import Database
 
 app = Flask(__name__)
-app.config.from_object('flashcash.config.DevelopmentConfig')
-app.config.from_pyfile('../instance/config.py')
+try:
+    app.config.from_pyfile('../instance/config.py')
+except:
+    print 'Warning: Using default config!'
+    app.config.from_object('flashcash.config.Config')
 
 db = Database(app)
